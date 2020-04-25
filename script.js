844ea58ec3dacefb2d3c53e847eee53677e8993c
VaderSteve76 <alverson.s@yahoo.com>
2020-04-25 17:34:44 -0400

    Add obj to dataArr, update DOM and format number as money functions

--- script.js
+++ script.js
@@ -52,6 +52,28 @@ function calculateWealth() {
   main.appendChild(wealthEl);
 };
 
+// Add new object to data arr
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+};
+
+// Update DOM
+function updateDOM(providedData = data) {
+  main.innerHTML = `<h2><strong>Person</strong>Wealth</h2>`;
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+};
+
+// Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};
+
 // EventListeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
